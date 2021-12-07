338f888a17854f1e1ec8c3421c42dde0f511f654
K-kiron <cielphantomhive2638@gmail.com>
2021-12-07 16:11:07 -0500

    update

---
+++ TP3-Yuchen XI_Yuyang XIONG_Wenhao XU/prototype/app/src/PlageHoraire.java
@@ -0,0 +1,17 @@
+public class PlageHoraire extends Date {
+
+	private String heure;
+
+	public String getHeure() {
+		return this.heure;
+	}
+
+	/**
+	 * 
+	 * @param Heure
+	 */
+	public void setHeure(String Heure) {
+		this.heure = Heure;
+	}
+
+}
