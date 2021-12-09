d75fe9fe15808717b645eeb279aadf92b89f5c32
K-kiron <cielphantomhive2638@gmail.com>
2021-12-09 15:41:43 -0500

    update

---
+++ TP2_Yuchen_HUI_Yuyang_XIONG_Wenhao_XU/prototype/app/src/ca/Benevole.java
@@ -0,0 +1,20 @@
+public class Benevole extends Equipier {
+
+	private Date[] listDispo;
+
+	public Benevole() {
+	}
+
+	public Benevole(String numeroDeCompte, String nom, String prenom, String motDePasse,
+					String codeIdentification, String adresse, String codePostal, String ville,
+					String adresseCourriel, String numeroDeTelephone) {
+		super(numeroDeCompte, nom, prenom, motDePasse, codeIdentification, adresse, codePostal,
+				ville, adresseCourriel, numeroDeTelephone);
+	}
+
+	public Date[] getList() {
+		// TODO - implement Benevole.getList
+		throw new UnsupportedOperationException();
+	}
+
+}
