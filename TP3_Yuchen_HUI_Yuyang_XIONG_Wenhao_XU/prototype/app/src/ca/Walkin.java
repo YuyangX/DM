f8d5883ecc3d4e9d80acdc5bdfbab82bb824f08b
K-kiron <cielphantomhive2638@gmail.com>
2021-12-09 15:37:01 -0500

    update

---
+++ TP3_Yuchen_HUI_Yuyang_XIONG_Wenhao_XU/prototype/app/src/ca/Walkin.java
@@ -0,0 +1,56 @@
+public class Walkin {
+
+	private String nom;
+	private String prenom;
+	private String heure;
+	private String tel;
+
+	public String getNom() {
+		return this.nom;
+	}
+
+	/**
+	 * 
+	 * @param nom
+	 */
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return this.prenom;
+	}
+
+	/**
+	 * 
+	 * @param prenom
+	 */
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getHeure() {
+		return this.heure;
+	}
+
+	/**
+	 * 
+	 * @param heure
+	 */
+	public void setHeure(String heure) {
+		this.heure = heure;
+	}
+
+	public String getTel() {
+		return this.tel;
+	}
+
+	/**
+	 * 
+	 * @param tel
+	 */
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+}
